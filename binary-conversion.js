4495425b935541d16d855b32ac698f1de91999e2
ethan <ethanmannette@gmail.com>
2016-01-08 19:09:29 -0700

    set up basic function layout, and module.exports for testing purposes

---
+++ binary-conversion.js
@@ -0,0 +1,32 @@
+// var userInput = document.getElementById('conversionString').value;
+// var convertOptions = document.getElementById('conversionOptions');
+
+function Convert(input, converting) {
+
+  switch (converting) {
+    case 'numbers':
+
+      break;
+    case 'letters':
+
+      break;
+    case 'numsAndLetters':
+
+      break;
+  }
+}
+
+function fromNums(input) {
+
+}
+
+function fromLetters(input) {
+
+}
+
+function fromNumsAndLetters(input) {
+
+}
+
+
+module.exports = Convert;
